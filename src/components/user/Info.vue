ee3d2fe14d9531cdeef718b05d3ad7e06cea479f
KomeijiLogi <zclxuan2012@163.com>
2017-12-30 16:03:47 +0800

    更新内容

--- src/components/user/Info.vue
+++ src/components/user/Info.vue
@@ -2,11 +2,14 @@
     <div class="info">
        <header class="header">
            <div class="header-icon">
-              <span class="iconfont2 "></span>
+              <span class="iconfont icon-account"></span>
            </div>
+           <span>用户名</span>
        </header>
        <div class="info-content">
-
+           <router-link :to="{name:''}">
+               <span></span>
+           </router-link>
        </div>
        <mt-button type="primary" size="large" @click="logout" >
           退出登录
@@ -39,16 +42,52 @@
   @import "../../assets/icons/iconfont";
   @import "../../assets/fz";
   @import "../../assets/icons2/iconfont";
-.info{
-   width: 100%;
-   padding-bottom: 16vw;
-  .header{
-     .header-icon{
 
+.info{
+  width: 100%;
+  padding-bottom: 14vw;
+  background-color: #F8FCFF;
+  .header{
+     width: 100%;
+     height: 16vw;
+     background: url(../../assets/carbg.png) center 0 #f37d0f;
+    background-size: auto 100%;
+    padding: 3.2vw 0;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+     .header-icon{
+       border: .4vw solid #ffffff;
+       background-color: @cl;
+       margin-left: 4vw;
+       -webkit-box-sizing: border-box;
+       box-sizing: border-box;
+       width: 14vw;
+       height: 14vw;
+       line-height: 16vw;
+       text-align: center;
+       border-radius: 50%;
+        span{
+          .fz(font-size,54);
+
+          &::before{
+            color: #ffffff;
+          }
+        }
      }
+    >span {
+      margin-left: 3.2vw;
+      .fz(font-size, 30);
+      color: #ffffff;
+      letter-spacing: .2vw;
+    }
   }
   .info-content{
 
   }
+
 }
 </style>
